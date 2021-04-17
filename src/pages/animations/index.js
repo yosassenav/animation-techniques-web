d8010c6f530d048495265a931e3c908a37bdf4ae
vanessa <mromerovanessa.soy@gmail.com>
2021-04-17 14:39:41 -0500

    Adding more components

--- src/pages/animations/index.js
+++ src/pages/animations/index.js
@@ -1,3 +1,14 @@
+import { Button } from "./../../components/button";
+
 export default function Animations() {
-  return <div>Animation page</div>;
+  return (
+    <div>
+      <h1>Animation page</h1>{" "}
+      <div>
+        <Button />
+        <Button />
+        <Button />
+      </div>
+    </div>
+  );
 }
